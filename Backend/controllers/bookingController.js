8b0fc4bb6b0ab5c8686966f2568d1def55dec87c
Tharindra <tharindragayashika@gmail.com>
2023-09-02 19:53:18 +0530

    Bookings Backend Routes
    Bookings Backend Routes are Created and Checked.

---
+++ Backend/controllers/bookingController.js
@@ -0,0 +1,22 @@
+
+
+export const getBookings = async (req, res, next) => {
+    res.json({mssg: 'getBooking is working'})   
+};
+
+export const getBooking = async (req, res, next) => {
+    res.json({mssg: 'getBookings is working'})
+};
+
+export const createBooking = async (req, res, next) => {
+    res.json({mssg: 'createBooking is working'})
+};
+
+export const updateBookin = async (req, res, next) => {
+    res.json({mssg: 'updateBookin is working'})
+};
+
+export const deleteBooking = async (req, res, next) => {
+    res.json({mssg: 'deleteBooking is working'})
+}
+
