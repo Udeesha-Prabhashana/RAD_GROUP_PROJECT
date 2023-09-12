bb55f6416a02c3cd8b4d6080f4627ea52c344bf9
Yuhanga Induwara <121022094+YuhangaInduwara@users.noreply.github.com>
2023-09-12 11:14:53 +0530

    Resolved errors

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,30 +6,9 @@ import CreatePayment from "./pages/payments/CreatePayment";
 import UpdatePayment from "./pages/payments/UpdatePayment";
 import Room from "./pages/rooms/Room";
 import BookingMain from "./pages/bookings/BookingMain";
-
+import FoodMain from "./pages/foods/FoodMain";
 import CustomerMain from "./pages/customers/CustomerMain";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/" element={<Login />} />
-                <Route path="/food" element={<Food />} />
-                <Route path="/addfood" element={<AddFoods />} />
-                <Route path="/editefood/:id" element={<EditeFood />} />
-                <Route path="/payment" element={<Payment />} />
-                <Route path="/createpayment" element={<CreatePayment />} />
-                <Route path="/updatepayment/:id" element={<UpdatePayment />} />
-                <Route path="/room" element={<Room />} />
-
-                <Route path="/bookings" element={<BookingMain />} />
-                <Route path="/customer" element={<CustomerMain />} />
-            </Routes>
-        </BrowserRouter>
-    );
-
-import FoodMain from "./pages/foods/FoodMain";
 
 function App() {
   return (
@@ -43,16 +22,13 @@ function App() {
         <Route path="/room" element={<Room />} />
         <Route path="/bookings" element={<BookingMain />} />
         <Route path="/food" element={<FoodMain />} />
+        <Route path="/customer" element={<CustomerMain />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
 
-//Tharindra is a simple human.
-//Sandaras is a simple Monstor.
-//Yuahnga is a simple human.
-//Nirasha is a simple Monstor.
 
 
 export default App;
+
